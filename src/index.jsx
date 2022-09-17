8ea372eb1ed599b99ca14f52ad7b241fddc9fa1c
Millene Prado <millene_vprado@hotmail.com>
2022-09-17 19:00:41 -0300

    created initial state

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,15 @@ import { createStore, combineReducers } from 'redux';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// initial state
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'paris'],
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
+
 // State and reducers
 const reducers = combineReducers({
   changeMe: (state = null, action) => state
