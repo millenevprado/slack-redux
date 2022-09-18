1ca171d152ca20216cc677980a09d723f14ca247
Millene Prado <millene_vprado@hotmail.com>
2022-09-18 17:51:24 -0300

    addes css properties for organizations sidebar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,8 +5,12 @@ import MessageList from '../containers/message_list';
 const App = () => {
   return (
     <div className="app">
+      <div className="organizations-sidebar">
+        <h2 className="organization active">LW</h2>
+      </div>
       <MessageList />
     </div>
+
   );
 };
 
