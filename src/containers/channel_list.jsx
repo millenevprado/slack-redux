ce00f7e51f9be87f0a78fd819f236de143781466
Millene Prado <millene_vprado@hotmail.com>
2022-09-19 09:35:18 -0300

    connect channels state from redux in the channel list container

--- src/containers/channel_list.jsx
+++ src/containers/channel_list.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class ChannelList extends Component {
   render() {
     return (
       this.props.channels.map((channel) => {
         return (
-          <div className="channel">
+          <div className="channel" key={channel}>
             <span>
               <i className="fab fa-slack-hash" />
               {channel}
@@ -15,6 +17,12 @@ class ChannelList extends Component {
       })
     );
   }
-};
+}
 
-export default ChannelList;
+function mapStateToProps(state) {
+  return {
+    channels: state.channels
+  };
+}
+
+export default connect(mapStateToProps, null)(ChannelList);
